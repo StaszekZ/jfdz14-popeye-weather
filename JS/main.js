f7a5b3194125fe9f1f29d151cd54dbcf21b1260a
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-06-07 18:15:04 +0200

    next chnage in game

--- JS/main.js
+++ JS/main.js
@@ -1,176 +1,176 @@
-const card1 = document.querySelector('.card1');
-const card2 = document.querySelector('.card2');
-const card3 = document.querySelector('.card3');
-const card4 = document.querySelector('.card4');
-const card5 = document.querySelector('.card5');
+// const card1 = document.querySelector('.card1');
+// const card2 = document.querySelector('.card2');
+// const card3 = document.querySelector('.card3');
+// const card4 = document.querySelector('.card4');
+// const card5 = document.querySelector('.card5');
 
-const teamCard = document.querySelector('.team-card');
-
-
-let leftPosition = -1500;
-let position1 = -2150;
-let position2 = -1850;
-let position3 = -1680;
-let position4 = -1900;
-let position5 = -1500;
-// let leftPosition = -200;
-// let position1 = 100;
-// let position2 = -200;
-// let position3 = -200;
-// let position4 = -200;
-// let position5 = -200;
-let topPosition = 0;
-let wave = 0;
+// const teamCard = document.querySelector('.team-card');
 
 
-
-observer = new IntersectionObserver((entries) => {
-    if (entries[0].intersectionRatio > 0) {
-        const changePosition = () => {
-            if (leftPosition !== 0) {
-                if (window.outerWidth > 1400) {
-                    card1.style.position = 'absolute';
-                    card2.style.position = 'absolute';
-                    card3.style.position = 'absolute';
-                    card4.style.position = 'absolute';
-                    card5.style.position = 'absolute';
-                    leftPosition += 10;
-                    position1 += 10;
-                    card1.style.marginLeft = `${position1}px`;
-                    card1.style.marginTop = `${-55}px`;
-                    position2 += 12;
-                    card2.style.marginLeft = `${position2}px`;
-                    position3 += 15;
-                    card3.style.marginLeft = `${position3}px`;
-                    card3.style.marginTop = `${-55}px`
-                    position4 += 10;
-                    card4.style.marginLeft = `${position4}px`;
-                    card4.style.marginTop = `${topPosition}px`;
-                    topPosition -= -2.5;
-                    position5 += 12;
-                    card5.style.marginLeft = `${position5}px`;
-                    card5.style.marginTop = `${topPosition}px`;
-                }
-            } else {
-
-            }
-            if (leftPosition === 0) {
-                clearInterval(interval);
-            }
-        }
+// let leftPosition = -1500;
+// let position1 = -2150;
+// let position2 = -1850;
+// let position3 = -1680;
+// let position4 = -1900;
+// let position5 = -1500;
+// // let leftPosition = -200;
+// // let position1 = 100;
+// // let position2 = -200;
+// // let position3 = -200;
+// // let position4 = -200;
+// // let position5 = -200;
+// let topPosition = 0;
+// let wave = 0;
 
 
-        const interval = setInterval(changePosition, 100);
 
-    }
-})
+// observer = new IntersectionObserver((entries) => {
+//     if (entries[0].intersectionRatio > 0) {
+//         const changePosition = () => {
+//             if (leftPosition !== 0) {
+//                 if (window.outerWidth > 1400) {
+//                     card1.style.position = 'absolute';
+//                     card2.style.position = 'absolute';
+//                     card3.style.position = 'absolute';
+//                     card4.style.position = 'absolute';
+//                     card5.style.position = 'absolute';
+//                     leftPosition += 10;
+//                     position1 += 10;
+//                     card1.style.marginLeft = `${position1}px`;
+//                     card1.style.marginTop = `${-55}px`;
+//                     position2 += 12;
+//                     card2.style.marginLeft = `${position2}px`;
+//                     position3 += 15;
+//                     card3.style.marginLeft = `${position3}px`;
+//                     card3.style.marginTop = `${-55}px`
+//                     position4 += 10;
+//                     card4.style.marginLeft = `${position4}px`;
+//                     card4.style.marginTop = `${topPosition}px`;
+//                     topPosition -= -2.5;
+//                     position5 += 12;
+//                     card5.style.marginLeft = `${position5}px`;
+//                     card5.style.marginTop = `${topPosition}px`;
+//                 }
+//             } else {
 
-let upWave = 0;
-
-const waveUpFunc = () => {
-    if (leftPosition === 0) {
-        wave++;
-        if (wave % 2 === 0) {
-            card1.style.marginTop = `${upWave - 55}px`;
-            card1.style.transform = 'scale(1.001)';
-            card2.style.marginTop = `${upWave}px`;
-            card2.style.transform = 'scale(0.99)';
-            card3.style.marginTop = `${upWave - 55}px`;
-            card3.style.transform = 'scale(1.001)';
-            card4.style.marginTop = `${upWave + 375}px`;
-            card4.style.transform = 'scale(0.99)';
-            card5.style.marginTop = `${upWave + 375}px`;
-            card5.style.transform = 'scale(0.99)';
-            upWave += 12;
-        } else {
-            card1.style.marginTop = `${upWave - 55}px`;
-            card1.style.transform = 'scale(0.99)';
-            card2.style.marginTop = `${upWave}px`;
-            card2.style.transform = 'scale(1.001)';
-            card3.style.marginTop = `${upWave - 55}px`;
-            card3.style.transform = 'scale(0.99)';
-            card4.style.marginTop = `${upWave + 375}px`;
-            card4.style.transform = 'scale(1.001)';
-            card5.style.marginTop = `${upWave + 375}px`;
-            card5.style.transform = 'scale(1.001)';
-            upWave -= 12;
-        }
-    }
-
-}
-
-const waveUpFuncInterval = setInterval(waveUpFunc, 750)
-observer.observe(teamCard);
-
-const bubbleWater = document.querySelectorAll('.team_bubble-water');
-
-let bubbleOpacity1 = 1;
-let bubbleTop1 = 0;
-let bubbleOpacity2 = 1;
-let bubbleTop2 = 0;
-let bubbleOpacity3 = 1;
-let bubbleTop3 = 0;
-let bubbleOpacity4 = 1;
-let bubbleTop4 = 0;
-let bubbleOpacity5 = 1;
-let bubbleTop5 = 0;
-
-const bubbleWaterTime = () => {
-    if (leftPosition === 0) {
-        for (let i = 0; i < 1; i++) {
-            bubbleTop1 -= 4;
-            bubbleWater[0].style.display = 'inline'
-            bubbleWater[0].style.marginTop = `${bubbleTop1}px`;
-            bubbleWater[0].style.opacity = bubbleOpacity1;
-            bubbleOpacity1 = bubbleOpacity1 - 0.085;
-            bubbleTop2 -= 4;
-            bubbleWater[1].style.display = 'inline'
-            bubbleWater[1].style.marginTop = `${bubbleTop2}px`;
-            bubbleWater[1].style.opacity = bubbleOpacity2;
-            bubbleOpacity2 = bubbleOpacity2 - 0.085;
-            bubbleTop3 -= 4;
-            bubbleWater[2].style.display = 'inline'
-            bubbleWater[2].style.marginTop = `${bubbleTop3}px`;
-            bubbleWater[2].style.opacity = bubbleOpacity3;
-            bubbleOpacity3 = bubbleOpacity3 - 0.085;
-            bubbleTop4 -= 4;
-            bubbleWater[3].style.display = 'inline'
-            bubbleWater[3].style.marginTop = `${bubbleTop4}px`;
-            bubbleWater[3].style.opacity = bubbleOpacity4;
-            bubbleOpacity4 = bubbleOpacity4 - 0.085;
-            bubbleTop5 -= 4;
-            bubbleWater[4].style.display = 'inline'
-            bubbleWater[4].style.marginTop = `${bubbleTop5}px`;
-            bubbleWater[4].style.opacity = bubbleOpacity5;
-            bubbleOpacity5 = bubbleOpacity5 - 0.085;
-            if (bubbleOpacity1 <= 0) {
-                bubbleOpacity1 = 1;
-                bubbleTop1 = 0;
-                bubbleOpacity2 = 1;
-                bubbleTop2 = 0;
-                bubbleOpacity3 = 1;
-                bubbleTop3 = 0;
-                bubbleOpacity4 = 1;
-                bubbleTop4 = 0;
-                bubbleOpacity5 = 1;
-                bubbleTop5 = 0;
-            }
-        }
+//             }
+//             if (leftPosition === 0) {
+//                 clearInterval(interval);
+//             }
+//         }
 
 
-    }
-}
+//         const interval = setInterval(changePosition, 100);
+
+//     }
+// })
+
+// let upWave = 0;
+
+// const waveUpFunc = () => {
+//     if (leftPosition === 0) {
+//         wave++;
+//         if (wave % 2 === 0) {
+//             card1.style.marginTop = `${upWave - 55}px`;
+//             card1.style.transform = 'scale(1.001)';
+//             card2.style.marginTop = `${upWave}px`;
+//             card2.style.transform = 'scale(0.99)';
+//             card3.style.marginTop = `${upWave - 55}px`;
+//             card3.style.transform = 'scale(1.001)';
+//             card4.style.marginTop = `${upWave + 375}px`;
+//             card4.style.transform = 'scale(0.99)';
+//             card5.style.marginTop = `${upWave + 375}px`;
+//             card5.style.transform = 'scale(0.99)';
+//             upWave += 12;
+//         } else {
+//             card1.style.marginTop = `${upWave - 55}px`;
+//             card1.style.transform = 'scale(0.99)';
+//             card2.style.marginTop = `${upWave}px`;
+//             card2.style.transform = 'scale(1.001)';
+//             card3.style.marginTop = `${upWave - 55}px`;
+//             card3.style.transform = 'scale(0.99)';
+//             card4.style.marginTop = `${upWave + 375}px`;
+//             card4.style.transform = 'scale(1.001)';
+//             card5.style.marginTop = `${upWave + 375}px`;
+//             card5.style.transform = 'scale(1.001)';
+//             upWave -= 12;
+//         }
+//     }
+
+// }
+
+// const waveUpFuncInterval = setInterval(waveUpFunc, 750)
+// observer.observe(teamCard);
+
+// const bubbleWater = document.querySelectorAll('.team_bubble-water');
+
+// let bubbleOpacity1 = 1;
+// let bubbleTop1 = 0;
+// let bubbleOpacity2 = 1;
+// let bubbleTop2 = 0;
+// let bubbleOpacity3 = 1;
+// let bubbleTop3 = 0;
+// let bubbleOpacity4 = 1;
+// let bubbleTop4 = 0;
+// let bubbleOpacity5 = 1;
+// let bubbleTop5 = 0;
+
+// const bubbleWaterTime = () => {
+//     if (leftPosition === 0) {
+//         for (let i = 0; i < 1; i++) {
+//             bubbleTop1 -= 4;
+//             bubbleWater[0].style.display = 'inline'
+//             bubbleWater[0].style.marginTop = `${bubbleTop1}px`;
+//             bubbleWater[0].style.opacity = bubbleOpacity1;
+//             bubbleOpacity1 = bubbleOpacity1 - 0.085;
+//             bubbleTop2 -= 4;
+//             bubbleWater[1].style.display = 'inline'
+//             bubbleWater[1].style.marginTop = `${bubbleTop2}px`;
+//             bubbleWater[1].style.opacity = bubbleOpacity2;
+//             bubbleOpacity2 = bubbleOpacity2 - 0.085;
+//             bubbleTop3 -= 4;
+//             bubbleWater[2].style.display = 'inline'
+//             bubbleWater[2].style.marginTop = `${bubbleTop3}px`;
+//             bubbleWater[2].style.opacity = bubbleOpacity3;
+//             bubbleOpacity3 = bubbleOpacity3 - 0.085;
+//             bubbleTop4 -= 4;
+//             bubbleWater[3].style.display = 'inline'
+//             bubbleWater[3].style.marginTop = `${bubbleTop4}px`;
+//             bubbleWater[3].style.opacity = bubbleOpacity4;
+//             bubbleOpacity4 = bubbleOpacity4 - 0.085;
+//             bubbleTop5 -= 4;
+//             bubbleWater[4].style.display = 'inline'
+//             bubbleWater[4].style.marginTop = `${bubbleTop5}px`;
+//             bubbleWater[4].style.opacity = bubbleOpacity5;
+//             bubbleOpacity5 = bubbleOpacity5 - 0.085;
+//             if (bubbleOpacity1 <= 0) {
+//                 bubbleOpacity1 = 1;
+//                 bubbleTop1 = 0;
+//                 bubbleOpacity2 = 1;
+//                 bubbleTop2 = 0;
+//                 bubbleOpacity3 = 1;
+//                 bubbleTop3 = 0;
+//                 bubbleOpacity4 = 1;
+//                 bubbleTop4 = 0;
+//                 bubbleOpacity5 = 1;
+//                 bubbleTop5 = 0;
+//             }
+//         }
 
 
-const bubbleWaterTimeInterval = setInterval(bubbleWaterTime, 150);
+//     }
+// }
 
-// ============= slider ======================== //
 
-const buttonPrev = document.querySelector(".button__prev");
-const buttonNext = document.querySelector(".button__next");
-const slides = document.querySelectorAll('.slides');
+// const bubbleWaterTimeInterval = setInterval(bubbleWaterTime, 150);
 
-const slidesLength = slides.length;
+// // ============= slider ======================== //
+
+// const buttonPrev = document.querySelector(".button__prev");
+// const buttonNext = document.querySelector(".button__next");
+// const slides = document.querySelectorAll('.slides');
+
+// const slidesLength = slides.length;
 
 
 // console.log(slides)
@@ -217,18 +217,19 @@ class Game {
 
         startBtn.addEventListener('click', () => {
             this.player.timer();
-            clearInterval(waveUpFuncInterval);
-            clearInterval(bubbleWaterTimeInterval);
+            this.submarine.positionSubmarineInterval();
         });
+        this.player.submarineAttack();
 
-        const submarineCard = document.querySelectorAll('.team-card');
 
-        for(let i = 0; i < submarineCard.length; i++){
-            submarineCard[i].addEventListener('click', () => {
-                this.submarine.gotHit();
-                console.log('klikam w attack');
-            })
-        }
+    }
+    restart(){
+        this.player = new Player();
+        this.submarine = new Submarine();
+    }
+
+    finishGame(){
+
     }
 }
 
@@ -239,36 +240,84 @@ class Submarine {
     }
     gotHit(){
         this.score += 1;
-        this.scoreElement.textContent = this.score;
+        this.scoreElement.textContent = this.score;  
+    }
+    positionSubmarine(){
+        const submarineCard = document.querySelectorAll('.team-card');
+
+        for(let i = 0; i < submarineCard.length; i++){
+            submarineCard[i].style.position = 'absolute';
+            const newPosition = Math.random() * 500;
+            submarineCard[i].style.left = `${newPosition}px`;
+            submarineCard[i].style.top = `${newPosition}px`;
+        }  
     }
 
+    positionSubmarineInterval(){
+        setInterval(this.positionSubmarine, 500)
+    }
+    
 }
 
 class Player {
-    time = 60;
+    
 
     constructor(){
         this.timeElement = document.querySelector('.game-time');
-        
-
+        this.submarineCardAttack = document.querySelectorAll('.team-card');
+        this.submarineCardAttackBcg = document.querySelectorAll('.card-bcg');
+        this.submarine = new Submarine();
     }
 
     timer(){
+        this.time = 10;
         const timeCounter = () => {
             this.time--;
             this.timeElement.textContent = this.time;
             if(this.time === 0){
                 this.timeElement.textContent = 'KONIEC !';
                 clearInterval(timerCount);
-                
             }
         }
         const timerCount = setInterval(timeCounter, 1000);   
+    } 
+    submarineAttack(){
+        this.counter = 0;
+        for(let i = 0; i < this.submarineCardAttack.length; i++){
+            this.submarineCardAttack[i].addEventListener('click', () => {
+                this.submarineCardAttack[i].style.backgroundImage = 'url(../img/popeyecard.png)';
+                this.submarineCardAttackBcg[i].style.display = 'none';
+                this.submarine.gotHit(); 
+                setInterval(() =>{
+                    if(this.counter % 2 === 0){
+                        this.submarineCardAttack[i].classList.add('none');
+                        this.counter++;
+                    } else {
+                        this.submarineCardAttack[i].classList.remove('none');
+                        this.counter++;
+                    }
+                }, 1000)                 
+            });
+            
+        }
     }
-    
 
-    
+
+    // changeCHaracter(){
+    //     setInterval(() => {
+    //         if(this.counter % 2 === 0){
+    //             this.submarineCardAttack[1].style.backgroundImage = 'url(../img/teamphoto/agnieszka.png)';
+    //             this.submarineCardAttack[2].style.backgroundImage = 'url(../img/teamphoto/kasia.png)';
+    //             this.submarineCardAttack[3].style.backgroundImage = 'url(../img/teamphoto/mateusz.png)';
+    //             this.submarineCardAttack[4].style.backgroundImage = 'url(../img/teamphoto/staszek.png)';
+    //             this.submarineCardAttack[5].style.backgroundImage = 'url(../img/teamphoto/lukasz.png)';
+    //             this.counter++;
+    //             console.log(this.counter);
+                
+    //     }}, 1000);
+    // }
 }
 
 const game = new Game();
 game.runGame();
+
