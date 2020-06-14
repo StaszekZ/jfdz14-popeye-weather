7816cad29c604fbe2eca81b9cbe7bdb78711e014
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2020-06-14 22:30:15 +0200

    stylix buttons and modal

--- JS/functionalities.js
+++ JS/functionalities.js
@@ -3,7 +3,8 @@
 // const boxes = [...document.querySelectorAll('.functionality__box')];
 // console.log(buttons);
 const background = document.querySelector('#functionality');
-
+const sections = document.querySelectorAll('section');
+const formBox = document.querySelector('.form__box');
 
 document.querySelector('body').addEventListener('click', function (e) {
   if (e.target.className.match('functionality__button')) {
@@ -17,29 +18,33 @@ document.querySelector('body').addEventListener('click', function (e) {
     // console.log(absoluteElementTop);
     // console.log(elementRect.top);
     // console.log(window.pageYOffset);
+    const fromTop = parent.getBoundingClientRect().top;
+    console.log(fromTop);
+    console.log(scrollY);
 
-    window.scrollTo(0, pageYOffset);
-
-    // parent.scrollIntoView()
-    // {
-    //   behavior: "smooth",
-    //   block: "center",
-    //   inline: "center"
-    // });
-
+    if (scrollY < 2000) {
+      window.scrollTo(0, scrollY + 480);
+    } else {
+      window.scrollTo(0, scrollY);
+    }
     parent.classList.toggle('active3');
     parent.children[2].classList.toggle('active__description');
-    document.querySelector('.gradient').classList.toggle('overlay');
+    // document.querySelector('.gradient').classList.toggle('overlay');
 
     if (e.target.textContent === 'Show More') {
       e.target.textContent = 'Show Less';
       let div = document.createElement('div');
       div.classList.add('back');
       background.appendChild(div);
+      formBox.style.zIndex = -10;
+      // sections.forEach(section => {
+      //   section.style.zIndex = -10;
+      // });
     } else {
       e.target.textContent = 'Show More';
       let div = document.querySelector('.back');
       background.removeChild(div);
+      formBox.style.zIndex = 1;
 
     }
   } else {
