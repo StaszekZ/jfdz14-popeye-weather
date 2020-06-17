07fdd551b9756158181bac38299caac80c825f95
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2020-06-17 10:56:11 +0200

    removed unnecessary lines in js functionalities

--- JS/functionalities.js
+++ JS/functionalities.js
@@ -1,7 +1,3 @@
-// const descriptions = document.querySelectorAll('.functionality__description');
-// const buttons = document.querySelectorAll(".functionality__button");
-// const boxes = [...document.querySelectorAll('.functionality__box')];
-// console.log(buttons);
 const background = document.querySelector('#functionality');
 const sections = document.querySelectorAll('section');
 const formBox = document.querySelector('.form__box');
@@ -10,14 +6,6 @@ document.querySelector('body').addEventListener('click', function (e) {
   if (e.target.className.match('functionality__button')) {
     const parent = e.target.parentElement;
     console.log(parent);
-
-    // const elementRect = parent.getBoundingClientRect();
-    // const absoluteElementTop = window.pageYOffset + elementRect.top;
-    // const middle = absoluteElementTop - (window.innerHeight / 2);
-    // console.log(middle);
-    // console.log(absoluteElementTop);
-    // console.log(elementRect.top);
-    // console.log(window.pageYOffset);
     const fromTop = parent.getBoundingClientRect().top;
     console.log(fromTop);
     console.log(scrollY);
@@ -29,7 +17,6 @@ document.querySelector('body').addEventListener('click', function (e) {
     }
     parent.classList.toggle('active3');
     parent.children[2].classList.toggle('active__description');
-    // document.querySelector('.gradient').classList.toggle('overlay');
 
     if (e.target.textContent === 'Show More') {
       e.target.textContent = 'Show Less';
@@ -37,9 +24,7 @@ document.querySelector('body').addEventListener('click', function (e) {
       div.classList.add('back');
       background.appendChild(div);
       formBox.style.zIndex = -10;
-      // sections.forEach(section => {
-      //   section.style.zIndex = -10;
-      // });
+
     } else {
       e.target.textContent = 'Show More';
       let div = document.querySelector('.back');
