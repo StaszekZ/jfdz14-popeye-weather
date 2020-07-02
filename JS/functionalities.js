48bcf5698b8ee0a72f074e6a2df6026ca11be239
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2020-07-02 12:58:51 +0200

    fixing pop-up window for Show More

--- JS/functionalities.js
+++ JS/functionalities.js
@@ -1,39 +1,18 @@
-const background = document.querySelector('#functionality');
-const sections = document.querySelectorAll('section');
-const formBox = document.querySelector('.form__box');
+const description = document.querySelector(".modal");
+const modalText = document.querySelector(".modalText");
+const buttons = document.querySelectorAll(".functionality__button");
 
-document.querySelector('body').addEventListener('click', function (e) {
-  if (e.target.className.match('functionality__button')) {
-    const parent = e.target.parentElement;
-    console.log(parent);
-    const fromTop = parent.getBoundingClientRect().top;
-    console.log(fromTop);
-    console.log(scrollY);
+buttons.forEach(function (button, index) {
+  button.addEventListener("click", (e) => {
+    console.log(e.target);
 
-    if (scrollY < 2000) {
-      window.scrollTo(0, scrollY + 500);
-    } else {
-      window.scrollTo(0, scrollY);
-    }
-    parent.classList.toggle('active3');
-    parent.children[2].classList.toggle('active__description');
-    parent.children[0].classList.toggle('hide__img');
+    description.style.display = "block";
+    modalText.setAttribute("data-i18n", `func.fun${index + 1}`);
+  });
+});
 
-    if (e.target.textContent === 'Show More') {
-      e.target.textContent = 'Show Less';
-      let div = document.createElement('div');
-      div.classList.add('back');
-      background.appendChild(div);
-      formBox.style.zIndex = -10;
-
-    } else {
-      e.target.textContent = 'Show More';
-      let div = document.querySelector('.back');
-      background.removeChild(div);
-      formBox.style.zIndex = 1;
-
-    }
-  } else {
-    console.error(`not a button!`);
-  }
+const closeModal = document.querySelector(".close");
+closeModal.addEventListener("click", () => {
+  description.style.display = "none";
+  modalText.removeAttribute("data-i18n");
 });
