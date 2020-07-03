77b8321da9252fb60a2054eb112de678fd943c0e
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2020-07-03 12:29:26 +0200

    final fix of modal popup for functionality

--- JS/functionalities.js
+++ JS/functionalities.js
@@ -1,18 +1,28 @@
 const description = document.querySelector(".modal");
-const modalText = document.querySelector(".modalText");
+const modalText = document.querySelector(".modal__text");
 const buttons = document.querySelectorAll(".functionality__button");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".close");
 
 buttons.forEach(function (button, index) {
   button.addEventListener("click", (e) => {
-    console.log(e.target);
-
+    console.log(i18next);
     description.style.display = "block";
     modalText.setAttribute("data-i18n", `func.fun${index + 1}`);
+    i18next.changeLanguage().then(function () {
+      localize();
+    });
   });
 });
 
-const closeModal = document.querySelector(".close");
 closeModal.addEventListener("click", () => {
   description.style.display = "none";
   modalText.removeAttribute("data-i18n");
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    description.style.display = "none";
+    modalText.removeAttribute("data-i18n");
+  }
 });
